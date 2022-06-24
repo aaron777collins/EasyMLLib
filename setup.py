51eb4fc084eba6d43be7ccd66333e78fa916167b
aaron <aaron777collins@gmail.com>
2022-06-24 18:47:14 -0400

    packaged!

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1' 
-DESCRIPTION = 'Easy_ML'
-LONG_DESCRIPTION = 'Easy_ML - A library to make machine learning easier'
+DESCRIPTION = 'EasyMLLIB'
+LONG_DESCRIPTION = 'EasyMLLIB - A library to make machine learning easier'
 
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="EasyML", 
+        name="EasyMLLIB", 
         version=VERSION,
         author="Aaron Collins",
         author_email="aaron777collins@gmail.com",
@@ -17,7 +17,7 @@ setup(
         install_requires=['certifi','charset-normalizer','click','colorama','cycler','et-xmlfile','Flask','fonttools','idna','itsdangerous','Jinja2','joblib','kiwisolver','MarkupSafe','matplotlib','numpy','openpyxl','packaging','pandas','Pillow','pyparsing','python-dateutil','pytz','requests','scikit-learn','scikit-plot','scipy','six','threadpoolctl','urllib3','Werkzeug'], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
-        keywords=['python', 'Easy_ML', 'Machine Learning'],
+        keywords=['python', 'EasyMLLIB', 'Machine Learning'],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Science/Research",
