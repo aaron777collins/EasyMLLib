65d99b10dfab36180659b43619824c27d68e2dce
aaron777collins <aaron777collins@gmail.com>
2022-11-21 17:54:00 -0500

    fixed splitter not allowing for a different classifier name

--- EasyMLLib/DataSplitter.py
+++ EasyMLLib/DataSplitter.py
@@ -15,18 +15,17 @@ class DataSplitter:
         TEST_SPLIT_PERCENT = testSplitPercent
         VAL_TEST_SPLIT_PERCENT = valTestSplitPercent
         SEED=seed
-        CLASSIFIER_NAME=classifierName
-        
-    
+        self.classifierName=classifierName
+
+
     # returns [features, answers] (assumes classifier is a column (or the name is set to the classifier name))
     def getAllFeaturesAndAnswers(self, data:pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        return [data.drop([CLASSIFIER_NAME], axis=1), data[CLASSIFIER_NAME]]
-    
+        return [data.drop([self.classifierName], axis=1), data[self.classifierName]]
+
     #returns [X_train, Y_train, X_val, Y_val, X_test, Y_test]
     def getTrainValTestSplit(self, data:pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame,pd.DataFrame, pd.DataFrame]:
         features, answers = self.getAllFeaturesAndAnswers(data)
-        
+
         X_train, X_test, Y_train, Y_test = train_test_split(features, answers, test_size=TEST_SPLIT_PERCENT, random_state=SEED)
         X_val, X_test, Y_val, Y_test = train_test_split(X_test, Y_test, test_size=VAL_TEST_SPLIT_PERCENT, random_state=SEED)
         return (X_train, Y_train, X_val, Y_val, X_test, Y_test)
-        
--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0' 
+VERSION = '0.1.1'
 DESCRIPTION = 'EasyMLLIB'
 LONG_DESCRIPTION = 'EasyMLLIB - A library to make machine learning easier'
 
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="EasyMLLIB", 
+        name="EasyMLLIB",
         version=VERSION,
         author="Aaron Collins",
         author_email="aaron777collins@gmail.com",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=['certifi','charset-normalizer','click','colorama','cycler','et-xmlfile','Flask','fonttools','idna','itsdangerous','Jinja2','joblib','kiwisolver','MarkupSafe','matplotlib','numpy','openpyxl','packaging','pandas','Pillow','pyparsing','python-dateutil','pytz','requests','scikit-learn','scikit-plot','scipy','six','threadpoolctl','urllib3','Werkzeug'], # add any additional packages that 
+        install_requires=['certifi','charset-normalizer','click','colorama','cycler','et-xmlfile','Flask','fonttools','idna','itsdangerous','Jinja2','joblib','kiwisolver','MarkupSafe','matplotlib','numpy','openpyxl','packaging','pandas','Pillow','pyparsing','python-dateutil','pytz','requests','scikit-learn','scikit-plot','scipy','six','threadpoolctl','urllib3','Werkzeug'], # add any additional packages that
         # needs to be installed along with your package. Eg: 'caer'
-        
+
         keywords=['python', 'EasyMLLIB', 'Machine Learning'],
         classifiers= [
             "Development Status :: 3 - Alpha",
